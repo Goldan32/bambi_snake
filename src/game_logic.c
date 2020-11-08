81819b86d0f2f56c7fcb931ad4787720e2bcec2d
koren <koren@DESKTOP-JQMLU8R>
2020-11-08 18:55:44 +0100

    Creating game_logic files

---
+++ src/game_logic.c
@@ -0,0 +1,9 @@
+/*
+ * game_logic.c
+ *
+ *  Created on: 8 Nov 2020
+ *      Author: koren
+ */
+
+
+#include "game_logic.h"
---
+++ src/game_logic.h
@@ -0,0 +1,13 @@
+/*
+ * game_logic.h
+ *
+ *  Created on: 8 Nov 2020
+ *      Author: koren
+ */
+
+#ifndef SRC_GAME_LOGIC_H_
+#define SRC_GAME_LOGIC_H_
+
+
+
+#endif /* SRC_GAME_LOGIC_H_ */
